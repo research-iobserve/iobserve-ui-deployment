e1878f8643f79db4c2317fdb8a7e0f0679a90bbc
Mathis Neumann <mathis@simpletechs.net>
2016-06-13 13:13:45 +0200

    add serviceinstance and communicationinstance resources

---
+++ src/main/java/org/iobserve/resources/ServiceInstanceResource.java
@@ -0,0 +1,37 @@
+package org.iobserve.resources;
+
+import org.iobserve.models.dataaccessobjects.ServiceInstanceDto;
+import org.iobserve.services.ServiceInstanceService;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+
+/**
+ * @author Mathis Neumann <mne@informatik.uni-kiel.de>
+ */
+@Path("v1")
+@Produces(MediaType.APPLICATION_JSON)
+public class ServiceInstanceResource implements SystemComponentModelResource<ServiceInstanceDto> {
+
+    @Inject
+    private ServiceInstanceService service;
+
+
+    @GET
+    @Path("systems/{systemId}/serviceinstances")
+    public List<ServiceInstanceDto> getAllBySystem(@PathParam("systemId") String systemId) {
+        return this.service.findAllBySystem(systemId);
+    }
+
+    @GET
+    @Path("/serviceinstances/{id}")
+    public ServiceInstanceDto getById(@PathParam("id") String id) {
+        return this.service.findById(id);
+    }
+}
