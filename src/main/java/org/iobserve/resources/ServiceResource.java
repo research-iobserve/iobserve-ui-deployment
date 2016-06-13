19fc5d324b901fefdd578364234c3bbe98effd0d
Mathis Neumann <mathis@simpletechs.net>
2016-06-13 13:04:21 +0200

    add services resource

---
+++ src/main/java/org/iobserve/resources/ServiceResource.java
@@ -0,0 +1,34 @@
+package org.iobserve.resources;
+
+import org.iobserve.models.dataaccessobjects.ServiceDto;
+import org.iobserve.services.ServiceService;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+
+/**
+ * @author Mathis Neumann <mne@informatik.uni-kiel.de>
+ */
+@Path("v1")
+@Produces(MediaType.APPLICATION_JSON)
+public class ServiceResource implements SystemComponentModelResource<ServiceDto> {
+
+    @Inject
+    private ServiceService service;
+
+
+    @GET
+    @Path("systems/{systemId}/services")
+    public List<ServiceDto> getAllBySystem(@PathParam("systemId") String systemId) {
+        return this.service.findAllBySystem(systemId);
+    }
+
+    @GET
+    @Path("/services/{id}")
+    public ServiceDto getById(@PathParam("id") String id) {
+        return this.service.findById(id);
+    }
+}
