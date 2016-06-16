509ecba91dd81fd7849962fb0a3ab6bb67e437cc
Mathis Neumann <mathis@simpletechs.net>
2016-06-16 17:53:41 +0200

    use local java api port

--- app/adapters/_baseAdapter.js
+++ app/adapters/_baseAdapter.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import RESTAdapter from 'ember-data/adapters/rest';
 
 export default RESTAdapter.extend({
-    host: 'http://localhost:3000',
+    host: 'http://localhost:8080',
     namespace: 'v1',
     session: Ember.inject.service()
 });
