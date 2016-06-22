dc25aae053fd760c6c3e5dbfe5600284e97c6ebb
Christoph Dornieden <cdor@informatik.uni-kiel.de>
2016-06-22 13:37:37 +0200

    added more styles

--- app/components/architecture-visualisation-cytoscape/themes.js
+++ app/components/architecture-visualisation-cytoscape/themes.js
@@ -10,5 +10,55 @@ export default {
         arrowBorderColor: '#3399CC',
         arrowColor: '#3399CC',
         arrowLabelColor: 'black'
+    },
+    'orange' :{
+        nodeGroupTextColor : '#002A4A',
+        nodeGroupColor : 'white',
+        borderColor : '#D64700',
+        nodeTextColor : '#002A4A',
+        nodeColor : '#17607D',
+        serviceColor : '#ED8910',
+        serviceTextColor : '#FFFEED',
+        arrowBorderColor : '#002A4A',
+        arrowColor : '#002A4A',
+        arrowLabelColor: 'black'
+    },
+    'forrest' :{
+        nodeGroupTextColor : '#36231B',
+        nodeGroupColor : 'white',
+        borderColor : '#60311F',
+        nodeTextColor : '#36231B',
+        nodeColor : '#8C5D42',
+        serviceColor : '#93A608',
+        serviceTextColor : '#F0E3CA',
+        arrowBorderColor : '#36231B',
+        arrowColor : '#36231B',
+        arrowLabelColor: '#93A608'
+    },
+    'maritim' :{
+        nodeGroupTextColor : '#1D3757',
+        nodeGroupColor : 'white',
+        borderColor : '#1D3757',
+        nodeTextColor : '#1D3757',
+        nodeColor : '#91BED4',
+        serviceColor : '#F29F01',
+        serviceTextColor : '#D9E8F5',
+        arrowBorderColor : '#1D3757',
+        arrowColor : '#1D3757',
+        arrowLabelColor: '#1D3757'
+    },
+    'green' :{
+        nodeGroupTextColor : '#403E21',
+        nodeGroupColor : 'white',
+        borderColor : '#403E21',
+        nodeTextColor : '#403E21',
+        nodeColor : '#70732F',
+        serviceColor : '#AABF8A',
+        serviceTextColor : '#403E21',
+        arrowBorderColor : '#89888C',
+        arrowColor : '#89888C',
+        arrowLabelColor: '#403E21'
     }
+
+
 };
