056716cc5e5d374a104a6fb84e35492385ea8076
Mathis Neumann <mathis@simpletechs.net>
2016-07-09 18:13:50 +0200

    no longer use fixtures

--- app/adapters/_baseAdapter.js
+++ app/adapters/_baseAdapter.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import RESTAdapter from 'ember-data/adapters/rest';
 import FixtureAdapter from 'ember-data-fixture-adapter';
 
-export default FixtureAdapter.extend({
+export default RESTAdapter.extend({
     host: 'http://localhost:8080', // TODO: replace with live url
     namespace: 'v1',
     session: Ember.inject.service()
