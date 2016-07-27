8a2a0dbe619830f5cd6d4b054351118f50817667
Mathis Neumann <mathis@simpletechs.net>
2016-07-27 12:24:46 +0200

    ignore changelogSequence and revisionNumber in entity-details

--- app/components/entity-details.js
+++ app/components/entity-details.js
@@ -1,20 +1,31 @@
 import Ember from 'ember';
 
+const { computed } = Ember;
+
 export default Ember.Component.extend({
     entity: null,
-
+    ignoredProperties: [
+        'Id$',
+        'timeSeries',
+        'statusInformations',
+        'revisionNumber',
+        'changelogSequence'
+    ],
     filteredProperties: Ember.computed('entity', 'entity._updated', function(){
         this.debug('entity', this.get('entity'));
         const properties = [];
         const entity = this.get('entity');
         entity.eachAttribute(property => {
             // do not show internal relations, also timeSeries gets plotted
-            if(property.indexOf('Id') < 0 && property !== 'timeSeries' && property !== 'statusInformations') {
+            if(!this.get('ignoredRegexp').some(regex => regex.test(property))) {
                 properties.push({key: property, value: entity.get(property)});
             }
         });
         this.debug('properties', properties);
         return properties;
+    }),
+    ignoredRegexp: computed('ignoredProperties', function() {
+        return this.get('ignoredProperties').map(prop => new RegExp(prop));
     })
 
 });
