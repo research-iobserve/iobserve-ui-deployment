94287919363f7be78996ee9e7e13072e80ea6d2c
Mathis Neumann <mathis@simpletechs.net>
2016-08-01 13:43:10 +0200

    reset current tab when data changes and add documentation

--- app/components/dynamic-entity-details.js
+++ app/components/dynamic-entity-details.js
@@ -1,30 +1,71 @@
 import Ember from 'ember';
 
 const { Component, computed, observer /*, on */ } = Ember;
-
+/**
+ * Displays tabs which contain the dynamic information nested within any `Measurable` entity, `statusInformations` and `timeSeries`.
+ * Will also handle the case where there is no information available.
+ *
+ * @class DynamicEntityDetailsComponent
+ * @extends Ember.Component
+ */
 export default Component.extend({
+    /**
+     * the model instance for the entity which can contain the `timeSeries` and `statusInformations` properties
+     * @type {Measurable}
+     * @property entity
+     * @required
+     * @public
+     */
     entity: null,
+
+    /**
+     * stores which time series tab is active (if `mode == series`)
+     * @type {Number}
+     * @private
+     */
     currentSeriesTab: 0,
+
+    /**
+     * store the mode, either `series` or `statusInformations'
+     * @type {String}
+     * @default series
+     * @private
+     */
     currentMode: 'series',
     init() {
-        if(this.get('entity.statusInformations')) {
+        const statusInfo = this.get('entity.statusInformations');
+        if(statusInfo && statusInfo.length > 0) {
             this.set('currentMode', 'statusInformations');
         } else {
             this.set('currentMode', 'series');
         }
         this._super(...arguments);
     },
-    updateDefaultTab: observer('entity.statusInformations', function() {
+    /**
+     * observer that gets called whenever the `statusInformations` or entity changes.
+     * Resets the current clicked tab so that the default open tab always contains visible data.
+     *
+     * @method updateDefaultTab
+     * @private
+     */
+    updateDefaultTab: observer('entity.statusInformations', 'entity.timeSeries', function() {
         const statusInfo = this.get('entity.statusInformations');
         if(statusInfo && statusInfo.length > 0) { // will load asynchronously (findRecord)
             this.set('currentMode', 'statusInformations');
         } else {
             this.set('currentMode', 'series');
         }
+        this.set('currentSeriesTab', 0);
     }),
-    isCurrentSeriesTab: observer('currentSeriesTab', 'currentMode', function(index) {
-        return this.get('currentMode') === 'series' && this.get('currentSeriesTab') === index;
-    }),
+
+    /**
+     * Computed property which returns the currently selected series element, if open.
+     *
+     * @property currentSeries
+     * @return {TimeSeriesElement} `undefined` if no series selected
+     * @private
+     * @readOnly
+     */
     currentSeries: computed('entity.timeSeries', 'currentSeriesTab', 'currentMode', function() {
         if(this.get('currentMode') !== 'series') {
             return false;
@@ -32,9 +73,26 @@ export default Component.extend({
         const allSeries = this.get('entity.timeSeries');
         return allSeries && allSeries[this.get('currentSeriesTab')];
     }),
+
+    /**
+     * Computed property which returns whether or not the currently open tab is statusInformations
+     *
+     * @property statusInformationsSelected
+     * @return {Boolean} is open
+     * @private
+     * @readOnly
+     */
     statusInformationsSelected: computed('currentMode', function() {
         return this.get('currentMode') === 'statusInformations';
     }),
+
+    /**
+     * Computed property which return whether or not data is available (`timeSeries` or `statusInformations`)
+     * @property isDataAvailable
+     * @return {Boolean}
+     * @private
+     * @readOnly
+     */
     isDataAvailable: computed('entity.{statusInformations,timeSeries}', function() {
         return !!(this.get('currentSeries') || this.get('statusInformationsSelected'));
     }),
