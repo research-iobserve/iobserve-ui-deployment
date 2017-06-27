d3075919583b1340aed9f0fcab6e385dbe899836
jweg <jweg@informatik.uni-kiel.de>
2017-06-27 09:51:39 +0200

    POST for creating system added

--- backend/src/main/java/org/iobserve/resources/SystemsResource.java
+++ backend/src/main/java/org/iobserve/resources/SystemsResource.java
@@ -1,24 +1,31 @@
 package org.iobserve.resources;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import org.iobserve.models.dataaccessobjects.RevisionDto;
 import org.iobserve.models.dataaccessobjects.SystemDto;
 import org.iobserve.services.ChangelogService;
 import org.iobserve.services.SystemService;
 
-import javax.inject.Inject;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import java.util.List;
-
 @Path("v1")
 @Produces(MediaType.APPLICATION_JSON)
 public class SystemsResource {
 
-    private SystemService service;
-    private ChangelogService changelogService;
+    private final SystemService service;
+    private final ChangelogService changelogService;
 
     @Inject
-    public SystemsResource(SystemService service, ChangelogService changelogService) {
+    public SystemsResource(final SystemService service, final ChangelogService changelogService) {
         this.service = service;
         this.changelogService = changelogService;
     }
@@ -31,14 +38,20 @@ public class SystemsResource {
 
     @GET
     @Path("/systems/{systemId}")
-    public SystemDto getSystem(@PathParam("systemId") String id) {
+    public SystemDto getSystem(@PathParam("systemId") final String id) {
         return this.service.findById(id);
     }
 
     @GET
     @Path("/systems/{systemId}/revision")
-    public RevisionDto getLatestRevision(@PathParam("systemId") String id) {
-        return changelogService.getLatestRevision(id);
+    public RevisionDto getLatestRevision(@PathParam("systemId") final String id) {
+        return this.changelogService.getLatestRevision(id);
+    }
+
+    @POST
+    @Path("/systems/")
+    public void newSystem(@Valid final SystemDto system) throws ConstraintViolationException {
+        this.service.createSystem(system);
     }
 
 }
--- backend/src/main/java/org/iobserve/services/SystemService.java
+++ backend/src/main/java/org/iobserve/services/SystemService.java
@@ -1,44 +1,66 @@
 package org.iobserve.services;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.validation.ConstraintViolationException;
+
 import org.glassfish.hk2.api.ServiceLocator;
 import org.iobserve.models.System;
 import org.iobserve.models.dataaccessobjects.SystemDto;
 import org.iobserve.models.mappers.DtoToBasePropertyEntityMapper;
 import org.iobserve.models.mappers.EntityToDtoMapper;
 
-import javax.inject.Inject;
-import javax.persistence.EntityManagerFactory;
-
 /**
  * @author Mathis Neumann <mne@informatik.uni-kiel.de>
  */
-public class SystemService extends AbstractService<System,SystemDto> {
+public class SystemService extends AbstractService<System, SystemDto> {
 
     @Inject
-    public SystemService(EntityManagerFactory entityManagerFactory, EntityToDtoMapper modelToDtoMapper, ServiceLocator serviceLocator, DtoToBasePropertyEntityMapper dtoToBasePropertyEntityMapper) {
+    public SystemService(final EntityManagerFactory entityManagerFactory, final EntityToDtoMapper modelToDtoMapper,
+            final ServiceLocator serviceLocator, final DtoToBasePropertyEntityMapper dtoToBasePropertyEntityMapper) {
         super(entityManagerFactory, modelToDtoMapper, serviceLocator, dtoToBasePropertyEntityMapper);
     }
 
-    //    @Override
-//    public List<SystemDto> findAll() {
-//        Query query = entityManager.createQuery("SELECT e FROM System e");
-//        return this.transformModelToDto((List<System>) query.getResultList());
-//    }
+    // @Override
+    // public List<SystemDto> findAll() {
+    // Query query = entityManager.createQuery("SELECT e FROM System e");
+    // return this.transformModelToDto((List<System>) query.getResultList());
+    // }
 
-//    @Override
-//    public SystemDto findById(String id) {
-//        return modelToDtoMapper.transform(entityManager.find(System.class, id));
-//    }
+    // @Override
+    // public SystemDto findById(String id) {
+    // return modelToDtoMapper.transform(entityManager.find(System.class, id));
+    // }
 
     @Override
-    protected SystemDto transformModelToDto(System system) {
+    protected SystemDto transformModelToDto(final System system) {
         return this.modelToDtoMapper.transform(system);
     }
 
     @Override
-    protected System transformDtoToModel(SystemDto systemDto) {
-        final System system = dtoToBasePropertyEntityMapper.transform(systemDto);
+    protected System transformDtoToModel(final SystemDto systemDto) {
+        final System system = this.dtoToBasePropertyEntityMapper.transform(systemDto);
 
         return system;
+    }
+
+    public synchronized void createSystem(final SystemDto system) throws ConstraintViolationException {
+        final EntityManager entityManager = this.entityManagerFactory.createEntityManager();
+        final System oldSystem = entityManager.find(System.class, system.getId());
+        if (oldSystem != null) {
+            // TODO error handling: new system with id already used by another system
+        }
+        final System createdSystem = new System();
+        createdSystem.setId(system.getId());
+        createdSystem.setName(system.getName());
+
+        final EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        entityManager.persist(createdSystem); // saves all thanks to cascading
+        tx.commit();
+        entityManager.close();
+
     }
 }
