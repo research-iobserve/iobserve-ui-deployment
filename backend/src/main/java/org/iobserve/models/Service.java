604498eb2d10f1a7cbfc7b277a3e6f96b1a6baa5
jweg <jweg@informatik.uni-kiel.de>
2017-09-26 15:21:57 +0200

    @ManyToOne added

--- backend/src/main/java/org/iobserve/models/Service.java
+++ backend/src/main/java/org/iobserve/models/Service.java
@@ -1,61 +1,75 @@
 package org.iobserve.models;
 
-import org.iobserve.models.util.Measurable;
-
-import javax.persistence.*;
 import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.iobserve.models.util.Measurable;
 
 /**
  * @author Christoph Dornieden <cdor@informatik.uni-kiel.de>
  */
 @Entity
-public class Service extends Measurable{
-    private String  name;
+public class Service extends Measurable {
+    private String name;
     private String description;
 
-
-    @OneToMany(mappedBy = "service",cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "service", cascade = CascadeType.ALL)
     private List<ServiceInstance> instances;
+
+    @ManyToOne(cascade = { CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
+    @XmlTransient
+    @JoinColumn(name = "userGroup")
+    private UserGroup userGroup;
 
     public Service() {
         super();
     }
 
-    public Service(String name, String description) {
+    // public Service(final String id) {
+    // super();
+    // super.setId(id);
+    // }
+
+    public Service(final String name, final String description) {
         super();
         this.name = name;
         this.description = description;
     }
 
-    public Service(String name, String description, List<ServiceInstance> instances) {
+    public Service(final String name, final String description, final List<ServiceInstance> instances) {
         this.name = name;
         this.description = description;
         this.instances = instances;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
-    public void setName(String name) {
+    public void setName(final String name) {
         this.name = name;
     }
 
     public String getDescription() {
-        return description;
+        return this.description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription(final String description) {
         this.description = description;
     }
 
     public List<ServiceInstance> getInstances() {
-        return instances;
+        return this.instances;
     }
 
-    public void setInstances(List<ServiceInstance> instances) {
+    public void setInstances(final List<ServiceInstance> instances) {
         this.instances = instances;
     }
-
 
 }
